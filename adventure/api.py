f9edb5c855ec30080c8551335b04630fbaa21311
JoshHalvorson <joshhalvorson16@gmail.com>
2019-09-26 13:17:33 -0700

    Chang move endpoint return

--- adventure/api.py
+++ adventure/api.py
@@ -83,7 +83,7 @@ def move(request):
         return JsonResponse({'name':player.user.username, 'title':nextRoom.title, 'description':nextRoom.description, 'key': nextRoom.key, 'players':players, 'error_msg':""}, safe=True)
     else:
         players = room.playerNames(player_id)
-        return JsonResponse({'name':player.user.username, 'title':room.title, 'description':room.description, 'players':players, 'error_msg':"You cannot move that way."}, safe=True)
+        return JsonResponse({'name':player.user.username, 'title':room.title, 'description':room.description, 'key': room.key, 'players':players, 'error_msg':"You cannot move that way."}, safe=True)
 
 
 @csrf_exempt
